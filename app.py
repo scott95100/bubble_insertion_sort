41cf543db48d05b12d1a9c4a70abd8295f0f883a
scott95100 <scott95100@gmail.com>
2021-03-18 15:45:51 -0700

    add insertion sort function

--- app.py
+++ app.py
@@ -8,7 +8,23 @@ def bubble_sort(arr):
         for j in range(0 , n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j+ 1], arr[j]
-    return arr
+    return arr  
 
 print(bubble_sort([4,3,7, 1, 9, 6, 2, 8, 5]))
 
+#insertion sort
+def insertion_sort(arr):
+    operations = 0
+    for i in range(len(arr)):
+        num_we_are_on = arr[i]
+        j = i - 1
+        #do a check to make sure we do not go out of range
+        while j >= 0 and arr[j] > num_we_are_on:
+            operations += 1
+            arr[j + 1] = arr[j]
+            j-= 1
+        j += 1
+        arr[j] = num_we_are_on
+    return arr, operations
+
+print(insertion_sort([4, 6, 2, 9, 1, 8, 3, 5]))
